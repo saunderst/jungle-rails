1c01323e06f07b57b94ff14e46cb5ff91e5f2c98
saunderst <thomas130@gmx.com>
2018-04-27 15:26:37 -0400

    Admin Categories feature

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
   def index
-    @category = Category.all.order(name: :desc)
+    @category_counts = Category.includes(:products).map {|c| [c.name, c.products.size] }
   end
 
   def new
@@ -9,10 +9,10 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(params.require(:category).permit(:name))
 
     if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created.'
     else
       render :new
     end
