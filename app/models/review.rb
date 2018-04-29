0121934d38da8a909e18d464e59684a2f0507e28
saunderst <thomas130@gmx.com>
2018-04-29 18:26:51 -0400

    model for Reviews with seed data; display reviews with product detail

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ActiveRecord::Base
+  belongs_to :product
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+end
