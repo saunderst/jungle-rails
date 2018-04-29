8b69c9b58915a794545f746cf17eff0c6ee922d5
saunderst <thomas130@gmx.com>
2018-04-29 14:49:35 -0400

    Basic authentication for admin functions

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USERNAME'], password: ENV['HTTP_BASIC_AUTH_PASSWORD']
+
   def index
     @category_counts = Category.includes(:products).map {|c| [c.name, c.products.size] }
   end
